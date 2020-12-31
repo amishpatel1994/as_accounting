87aa1d82724d1daf80363463fe371eaca68c458e
Amish Patel <amishpatel94@hotmail.com>
2020-12-31 17:14:33 -0500

    Add copyright at the bottom

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,7 +4,7 @@ export default function PageFooter() {
   return (
     <div id="footer">
       <ul className="copyright">
-        <li>Made with &hearts; using gatsby, reactjs, and react-vertical-timeline-component</li>
+        <li>Copyright 2020, A.S. Accounting, All Rights Reserved</li>
       </ul>
     </div>
   );
